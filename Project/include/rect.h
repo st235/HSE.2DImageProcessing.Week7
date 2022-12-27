96be8cb2652b1ab678a27760e4cd5d8c0b528b96
Alexander Dadukin <hi@st235.xyz>
2022-12-27 10:50:49 +0000

    implemented rect class.

---
+++ Project/include/rect.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexander Dadukin on 27.12.2022.
+//
+
+#ifndef PROJECT_RECT_H
+#define PROJECT_RECT_H
+
+#endif //PROJECT_RECT_H
---
+++ Project/src/rect.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Alexander Dadukin on 27.12.2022.
+//
